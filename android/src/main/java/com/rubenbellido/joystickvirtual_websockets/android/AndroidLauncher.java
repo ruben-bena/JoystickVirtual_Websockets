0d0f98698263b581db9c8dff4b906924c04066e9
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-04-13 19:07:29 +0200

    Things that have to be done to use websockets, following wiki

--- android/src/main/java/com/rubenbellido/joystickvirtual_websockets/android/AndroidLauncher.java
+++ android/src/main/java/com/rubenbellido/joystickvirtual_websockets/android/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.czyzby.websocket.CommonWebSockets;
 import com.rubenbellido.joystickvirtual_websockets.Main;
 
 /** Launches the Android application. */
@@ -13,6 +14,7 @@ public class AndroidLauncher extends AndroidApplication {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
         configuration.useImmersiveMode = true; // Recommended, but not required.
+        CommonWebSockets.initiate();
         initialize(new Main(), configuration);
     }
 }
--- lwjgl3/src/main/java/com/rubenbellido/joystickvirtual_websockets/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/rubenbellido/joystickvirtual_websockets/lwjgl3/Lwjgl3Launcher.java
@@ -2,12 +2,14 @@ package com.rubenbellido.joystickvirtual_websockets.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.github.czyzby.websocket.CommonWebSockets;
 import com.rubenbellido.joystickvirtual_websockets.Main;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
+        CommonWebSockets.initiate();
         createApplication();
     }
 
